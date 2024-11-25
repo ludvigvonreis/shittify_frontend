88b72c06a9df88203ec665031f0bfd30eec63807
Ludvig <ludvigvonreis@gmail.com>
2024-11-25 23:41:50 +0100

    Added enqueueing of tracks found by search

--- src/routes/search/$query.tsx
+++ src/routes/search/$query.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { secondsToClockFormat } from "@utils/time";
-import { useSetQueue } from "@hooks/mediaHooks";
+import { useAddToQueue, useSetQueue } from "@hooks/mediaHooks";
 import { useAtom, useSetAtom } from "jotai";
 import { TogglesAtom } from "@atoms/MediaPlayerAtoms";
 import { SearchAtom } from "@components/search/SearchField";
 import { usePageTitle } from "@hooks/usePageTitle";
+import Icon from "@components/shared/Icon";
 
 export const Route = createFileRoute("/search/$query")({
 	component: RouteComponent,
@@ -21,7 +22,7 @@ async function getSearchResults(query: string) {
 
 		return data;
 	} catch (e) {
-		throw new Error("Uhh hawk tuah");
+		throw new Error("Uh Oh!");
 	}
 }
 
@@ -40,24 +41,28 @@ function RouteComponent() {
 	});
 
 	const setQueue = useSetQueue();
+	const addToQueue = useAddToQueue();
 	const setMediaToggles = useSetAtom(TogglesAtom);
 
-	if (!isSuccess) return <>Hawk tuh uh</>;
-	if (data.length < 1) return <>Melker</>;
+	if (data.length < 1) return <>Nothing Found</>;
 
 	return (
 		<>
 			<div className="flex flex-row p-5 gap-5 select-none">
 				<div className="flex bg-slate-800 w-1/3 h-64 rounded-md p-6 flex-col hover:bg-slate-700 transition-colors">
 					<img
-						className="w-32 h-32"
+						className="w-32 h-32 rounded-md"
 						src={data[0].album_image}
 						alt="image of {data[0].album_image}"
 					/>
 					<h2 className="text-3xl pt-2 truncate">
 						{data[0].track_name}
 					</h2>
-					<p className="text-slate-400"><span className="font-light">{"Track"}</span>{" • "}{data[0].artist_name}</p>
+					<p className="text-slate-400">
+						<span className="font-light">{"Track"}</span>
+						{" • "}
+						{data[0].artist_name}
+					</p>
 				</div>
 				<ul className="flex flex-col w-2/3">
 					{data.slice(0, 4).map((element) => {
@@ -67,8 +72,8 @@ function RouteComponent() {
 								className="hover:bg-slate-800 rounded-md h-16 flex flex-row 
 								items-center gap-3 p-1 w-ful transition-colors"
 								onDoubleClick={() => {
-									setQueue(
-										[{
+									setQueue([
+										{
 											...element,
 											artist: element.album_name,
 											album: element.album_name,
@@ -77,8 +82,8 @@ function RouteComponent() {
 											index: element.track_index,
 											path: element.track_path,
 											image: element.album_image,
-										}]
-									);
+										},
+									]);
 									setMediaToggles((toggles) => {
 										return { ...toggles, isPlaying: true };
 									});
@@ -97,7 +102,26 @@ function RouteComponent() {
 										{element.artist_name}
 									</p>
 								</div>
-								<p className="text-base ml-auto pr-4">
+								<button
+									title="Add to Queue"
+									className="justify-self-center cursor-pointer ml-auto
+										text-slate-300 hover:text-inherit"
+									onClick={() =>
+										addToQueue({
+											...element,
+											artist: element.album_name,
+											album: element.album_name,
+											name: element.track_name,
+											duration: element.track_duration,
+											index: element.track_index,
+											path: element.track_path,
+											image: element.album_image,
+										})
+									}
+								>
+									<Icon type={"queue_music"} />
+								</button>
+								<p className="text-base pr-4">
 									{secondsToClockFormat(
 										element.track_duration
 									)}
