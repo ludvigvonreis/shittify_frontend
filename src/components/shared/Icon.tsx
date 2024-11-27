6df8e848a9ef1b8bfc25024af0cdcb8ff8ab65f2
Ludvig <ludvigvonreis@gmail.com>
2024-11-27 11:34:39 +0100

    Added support for react spring to Icon component.

--- src/components/shared/Icon.tsx
+++ src/components/shared/Icon.tsx
@@ -1,21 +1,25 @@
+import { animated, AnimationProps, SpringValue } from "@react-spring/web";
+import { CSSProperties } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface IconProps {
+interface IconProps extends AnimationProps {
 	type: string;
 	className?: string;
 	title?: string;
 	onClick?: (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+	style?: React.CSSProperties | Record<string, SpringValue<any>>;
 }
 
 export default function Icon(props: IconProps) {
 	return (
-		<span
+		<animated.span
 			data-ignore-click
 			title={props.title}
 			className={twMerge("material-symbols-outlined inline align-bottom select-none", props.className)}
 			onClick={props.onClick}
+			style={props.style}
 		>
 			{props.type}
-		</span>
+		</animated.span>
 	);
 }
