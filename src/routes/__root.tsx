ebe70435a0eaadeb44fcb41ae8ca3a8f483c4c26
Ludvig <ludvigvonreis@gmail.com>
2024-12-05 20:52:36 +0100

    Added creating playlists from home menu, added showing user playlists

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,24 @@
-import { Link, Outlet, createRootRoute, useLocation } from "@tanstack/react-router";
+import {
+	Link,
+	Outlet,
+	createRootRoute,
+	useLocation,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import MediaPlayer from "@components/mediaPlayer/MediaPlayer";
 import SearchField from "@components/search/SearchField";
-import { QueryClientProvider, QueryClient, useQuery } from "@tanstack/react-query";
+import {
+	QueryClientProvider,
+	QueryClient,
+	useQuery,
+} from "@tanstack/react-query";
 import Icon from "@components/shared/Icon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Provider, useAtom, useAtomValue } from "jotai";
 import ProfileWrapper from "@components/auth/ProfileWrapper";
 import { SettingsAtom } from "@atoms/atoms";
 import { fetchWithAuth } from "@utils/fetchWithAuth";
+import { authClient, isAuthenticated } from "@lib/auth-client";
 
 export const Route = createRootRoute({
 	component: RootComponent,
@@ -31,16 +41,21 @@ function RootComponent() {
 	// HACK: Biggest hack ever, prevents stuff from loading in this ROOT component when login is visible.
 	// Both to prevent unused rendering time but also to prevent music and interaction from happening in the login route
 	const location = useLocation();
-	if (location.href.includes("login")) return <Outlet />;
+	if (location.href.includes("login"))
+		return (
+			<QueryClientProvider client={queryClient}>
+				<Outlet />
+			</QueryClientProvider>
+		);
 
 	return (
 		<>
 			<nav className="w-full h-20 grid grid-cols-3 grid-rows-1 p-2">
 				<Link to="/" className="justify-start px-5 flex items-center">
-				<Icon
-					type="graphic_eq"
-					className="text-[3rem] text-slate-200 hover:text-inherit transition-colorshover:text-inherit transition-colors"
-				/>
+					<Icon
+						type="graphic_eq"
+						className="text-[3rem] text-slate-200 hover:text-inherit transition-colorshover:text-inherit transition-colors"
+					/>
 				</Link>
 				<SearchField />
 				<div className="justify-end">
@@ -57,11 +72,13 @@ function RootComponent() {
 }
 
 function SettingsProvider() {
+	const [isAuth, setIsAuth] = useState(false);
 	const { data } = useQuery({
 		queryKey: ["settings"],
 		queryFn: () =>
 			fetchWithAuth<Settings>("http://localhost:3000/api/v1/settings"),
 		staleTime: 0,
+		enabled: isAuth,
 	});
 	const [settings, setSettings] = useAtom(SettingsAtom);
 
@@ -69,7 +86,13 @@ function SettingsProvider() {
 		if (!data) return;
 
 		setSettings(data);
-	}, [settings, data, setSettings])
+	}, [settings, data, setSettings]);
+
+	useEffect(() => {
+		(async () => {
+			setIsAuth(await isAuthenticated());
+		})();
+	}, [isAuth, setIsAuth]);
 
 	return <></>;
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,12 @@
-import * as React from "react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
 import { usePageTitle } from "@hooks/usePageTitle";
 import AudioVizualizer from "@components/filters/AudioVizualizer";
+import { SearchRow } from "./search/$query";
+import Card from "@components/search/Card";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { fetchWithAuth } from "@utils/fetchWithAuth";
 import { authClient } from "@lib/auth-client";
+import Icon from "@components/shared/Icon";
 
 export const Route = createFileRoute("/")({
 	component: HomeComponent,
@@ -10,9 +14,78 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
 	usePageTitle("Shitifiy");
+	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+
+	const user = authClient.useSession().data?.user;
+	const { data, isSuccess } = useQuery({
+		queryKey: ["userPlaylists"],
+		queryFn: () =>
+			fetchWithAuth<PlaylistMetadata[]>(
+				`http://localhost:3000/api/v1/user/${user?.id}/playlists`
+			),
+		enabled: !!user,
+	});
+
+	const mutation = useMutation({
+		mutationKey: ["createPlaylist"],
+		mutationFn: () =>
+			fetchWithAuth("http://localhost:3000/api/v1/playlist/new", {
+				method: "POST",
+			}),
+		onSuccess: (data) => {
+			queryClient.invalidateQueries({ queryKey: ["userPlaylists"] });
+			navigate({
+				to: "/playlist/$playlistid",
+				params: { playlistid: data.playlist_id },
+			});
+		},
+	});
+
+	if (!isSuccess) return <></>;
 
 	return (
 		<div className="p-2">
+			<SearchRow
+				title={
+					<div className="flex gap-5 w-full">
+						<h2 className="font-normal">Your Playlists</h2>
+						<button
+							className="ml-auto"
+							onClick={() => mutation.mutate()}
+						>
+							<h2 className="font-bold align-top text-slate-300 text-lg
+								hover:text-inherit bg-slate-700 rounded-xl py-2 px-4">
+								Create New Playlist
+							</h2>
+						</button>
+					</div>
+				}
+			>
+				{data.slice(0, 7).map((element) => (
+					<Card
+						key={element.playlist_id}
+						type={"Playlist"}
+						header={
+							<Link
+								to="/playlist/$playlistid"
+								params={{ playlistid: element.playlist_id }}
+							>
+								{element.name}
+							</Link>
+						}
+						subHeader={element.description}
+						image={element.image}
+						className={"min-w-[14.2857%]"} // 1 / 7 to 4 dec precision
+						onDoubleClick={() =>
+							navigate({
+								to: "/playlist/$playlistid",
+								params: { playlistid: element.playlist_id },
+							})
+						}
+					/>
+				))}
+			</SearchRow>
 			<AudioVizualizer className={"w-full h-64 rounded-lg mx-auto"} />
 		</div>
 	);
